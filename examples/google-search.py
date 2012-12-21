b1333ff7d6f3161d71d2efc281bf7018c23ba391
Dennis Ideler <ideler.dennis@gmail.com>
2012-12-20 21:46:32 -0500

    Print title in search example

--- examples/google-search.py
+++ examples/google-search.py
@@ -2,10 +2,13 @@
 # http://code.google.com/p/chromedriver/wiki/GettingStarted
 
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome() # Optional argument; will search PATH if not specified.
 driver.get('http://google.com/xhtml') # TODO: check if it works without xhtml
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
-search_box.submit()
+search_box.submit() # Google searches automatically anyway.
+time.sleep(1)
+print driver.title
 driver.quit()
