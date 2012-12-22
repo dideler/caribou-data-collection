4c62f40f6c1c0a6d37ebc9dadd3b0ff03f6ba694
Dennis Ideler <ideler.dennis@gmail.com>
2012-12-22 04:43:38 -0500

    Update BC script
    - Iteration of all cities now works
    - Improved logging
    - Added sanity checks (helps prevent scraping incorrect data)

--- canada/british-columbia/bc_schools.py
+++ canada/british-columbia/bc_schools.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2012  Dennis Ideler
+# Copyright (c) 2012  Dennis Ideler <ideler.dennis at gmail.com>
+# Prepared for Professor Thomas Wolf <twolf at brocku.ca>
+# School contact info to be used for the Caribou Cup Mathematics Competition.
 
 import argparse
 import logging
 from sys import argv
 from time import strftime
+#from time import sleep  # TODO: use to randomize crawling pattern and to cut the server some slack
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import UnexpectedTagNameException
 
 """Collects contact info from all schools in British Columbia.
 
@@ -27,6 +34,59 @@ def extract(url):
     """
     browser = webdriver.Chrome()
     browser.get(url)
+    assert 'School and District Contacts' in browser.title, 'Wrong webpage.'
+    # TODO
+    # find city-list element - x
+    # for each city - x
+    #   click city - x
+    #   find school-list element
+    #   for each school
+    #       click school
+    #       extract school name
+    #               address
+    #               city
+    #               province (will be 'BC')
+    #               postal code
+    #               school board
+    #               contact name
+    #               contact position (e.g. teacher or principal)
+    #               email
+    #               timezone (will be 'America/Vancouver')
+    #               country (will be 'CA')
+    
+    # NOTE: The page reloads on every option click. Therefore the element will
+    #       no longer exist in cache and Selenium will complain.
+    #       A workaround is to re-find the element after every option click.
+
+    # This is slightly more efficient than:
+    #   city_list = browser.find_element_by_id('citySelect')
+    #   city_options = city_list.find_elements_by_tag_name('option')
+    try:
+        city_list = Select(browser.find_element_by_id('citySelect'))
+    except UnexpectedTagNameException:
+        print 'Element found is not a SELECT tag.'
+    cities = [option.text.strip() for option in city_list.options]
+    num_cities = len(cities)
+    logging.info("Found %d cities", num_cities-1) # First option is not a city.
+
+    for i in xrange(1, num_cities):
+        while True: # This is a dirty hack since Selenium's wait methods aren't working.
+            try:
+                city_list = Select(browser.find_element_by_id('citySelect'))
+                expected_city, actual_city = cities[i], city_list.options[i].text.strip()
+                assert expected_city == actual_city, ('Wrong city selected. '
+                    'Expected: {}. Actual: {}.').format(expected_city, actual_city)
+                logging.info("Crawling city #%d: %s", i, actual_city)
+                city_list.select_by_index(i)
+            except StaleElementReferenceException:
+                # By the time it retries, the element should have loaded.
+                continue
+            break
+
+        # school list id -> citySchoolSelect
+
+    #browser.back()
+    browser.close()
 
 def set_logger(loglevel):
     """Sets up logging to a file.
